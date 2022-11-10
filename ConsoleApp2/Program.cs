423d2a2fad035e30cf22dd01dbe6afbc9046d181
123 <123@DESKTOP-6J147SD>
2022-11-10 23:07:47 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0 && number % 3 == 0)
+                Console.WriteLine("Число является четным и не оканчивается на цифру 3.");
+            else if (number % 2 == 0)
+                Console.WriteLine("Число является четным.");
+            else if (number % 3 == 0)
+                Console.WriteLine("Число оканчивается на цифру 3.");
+            else
+                Console.WriteLine("Число не является четным и не оканчивается на цифру 3.");
+
+            Console.ReadKey();
+        }
+    }
+}
